9e03a33293d9c3f3cd16d6e611e63d7b864e1c18
HEXCODE <rishadb7@gmail.com>
2024-06-06 14:50:56 +0530

    head make true
    head make true

--- index.js
+++ index.js
@@ -98,6 +98,82 @@ app.get('/it', async function (req, res) {
 })
 
 
+app.get('/it2', async function (req, res) {
+
+   
+    let browser;
+
+  
+    try {
+        browser = await puppeteer.launch({
+            headless: true,
+
+            args: ['--disable-web-security',
+                '--disable-features=IsolateOrigins,site-per-process',
+              //  `--proxy-server=${proxyServer}`
+            ]
+        });
+
+        const page = await browser.newPage();
+        await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36');
+        await page.setViewport({ width: 1366, height: 768 });
+        await page.setDefaultNavigationTimeout(60000);
+        await page.setDefaultTimeout(60000);
+
+
+
+        await page.goto('https://eportal.incometax.gov.in/iec/foservices/#/login', { waitUntil: 'domcontentloaded' });
+
+        await page.waitForSelector('#panAdhaarUserId');
+        await page.type('#panAdhaarUserId', 'DSYPP0141J', { delay: 100 });
+
+        await page.evaluate(() => {
+            document.querySelector('.large-button-primary.width.marTop16').click();
+        });
+
+        await page.waitForNavigation();
+        await page.screenshot({ path: 'password.png' });
+
+        await page.waitForSelector('#passwordCheckBox-input');
+
+        await page.evaluate(() => {
+            document.querySelector('#passwordCheckBox-input').click();
+        });
+
+        await page.waitForSelector('#loginPasswordField');
+        await page.type('#loginPasswordField', 'Pattanath7@', { delay: 0 });
+
+
+        await page.waitForSelector('.large-button-primary.width.marTop26');
+
+       // await page.authenticate('DSYPP0141J', 'Pattanath7@');
+
+        await page.evaluate(() => {
+            document.querySelector('.large-button-primary.width.marTop26').click();
+        });
+
+
+        const pageContent = await page.content();
+
+
+        res.set('Content-Type', 'text/html');
+        res.send(Buffer.from(pageContent));
+
+
+
+    } catch (error) {
+        console.error("Error encountered:", error);
+        if (browser) {
+            await browser.close();
+        }
+        // Send an error response
+        // Assuming `res` is defined in your actual code
+        res.status(500).send("An error occurred");
+    }
+
+
+})
+
 
 // Making Express listen on port 7000
 app.listen(7000, function () {
