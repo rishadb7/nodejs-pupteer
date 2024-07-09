27a7134b3961a38123813334f27cb713d6107725
Rishad <rishadb7@gmail.com>
2024-07-09 18:24:57 +0530

    Update index.js
    password updated

--- index.js
+++ index.js
@@ -186,7 +186,7 @@ app.get('/it2', async function (req, res) {
         });
 
         await page.waitForSelector('#loginPasswordField');
-        await page.type('#loginPasswordField', 'Pattanath7@', { delay: 0 });
+        await page.type('#loginPasswordField', '######', { delay: 0 });
 
 
         await page.waitForSelector('.large-button-primary.width.marTop26');
